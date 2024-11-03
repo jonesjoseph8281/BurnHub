947760c8c431fee6e129ea2f9eb7edc79cf1f337
jonesjoseph8281 <jonesjoseph8281@gmail.com>
2024-11-03 06:08:40 +0530

    fix

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,25 +1,17 @@
 import React from 'react'
-import axios from "axios";
 import './HeroStyles.css'
 import Video from '../../assets/background.mp4'
 import logo from '../../assets/logo.png'
 
 function Hero() {
-  const handleSignIn = async () => {
-    try {
-        const response = await axios.post('http://localhost:3000/request');
-        window.location.href = response.data.url; // Redirect to Google sign-in URL
-    } catch (error) {
-        console.error('Error during Google sign-in:', error);
-    }
-};
+
 
   return (
     <div className='hero'>
         <video autoPlay loop muted id='video'>
                 <source src={Video} type='video/mp4' />
         </video>
-        <div className="logo" onClick={() => handleSignIn()}>
+        <div className="logo">
                 <img src={logo} alt="BurnHub Logo" className="logo-image" />
         </div>
         <div className="content">
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
 import React, {useState} from 'react'
 import {BsPerson} from 'react-icons/bs'
+import axios from "axios";
 
 import './NavbarStyles.css';
 
 function Navbar() {
+  const handleSignIn = async () => {
+    try {
+        const response = await axios.post('http://localhost:3000/request');
+        window.location.href = response.data.url; // Redirect to Google sign-in URL
+    } catch (error) {
+        console.error('Error during Google sign-in:', error);
+    }
+};
         const[nav, setNav] = useState(false)
         const handleNav = () => setNav(!nav)
   return (
     <div className={nav ? 'navbar navbar-bg' : 'navbar'}>
-        <div className="nav-icons">
+        <div className="nav-icons" onClick={() => handleSignIn()}>
               <BsPerson className='icon' />  
         </div>
     </div>
