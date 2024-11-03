c4565934aec04d626c752fac8e56fe253eecd890
jonesjoseph8281 <jonesjoseph8281@gmail.com>
2024-11-03 08:59:55 +0530

    hopefully deployed

--- backend/src/routes/request.mjs
+++ backend/src/routes/request.mjs
@@ -8,10 +8,10 @@ dotenv.config();
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'http://burn-hub-frontend.vercel.app');
     res.header('Referrer-Policy', 'no-referrer-when-downgrade');
 
-    const redirectUrl = 'http://localhost:3000/auth/google/callback';
+    const redirectUrl = 'https://burn-hub.onrender.com/auth/google/callback';
     
     const oAuthClient = new OAuth2Client(
         process.env.CLIENT_ID,
